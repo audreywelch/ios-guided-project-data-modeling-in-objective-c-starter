ad7c5889132e7e180426822a4b9bea8ff7c5de47
Paul Solt <PaulSolt@gmail.com>
2020-04-15 14:09:39 -0400

    Finished parsing

--- Quakes-ObjcTests/LSIQuakeTests.m
+++ Quakes-ObjcTests/LSIQuakeTests.m
@@ -10,6 +10,7 @@
 #import "LSIFileHelper.h"
 #import "LSIQuake.h" // Rebuild Unit Test: Command + Shift + U
 #import "LSILog.h"
+#import "LSIQuakeResults.h"
 
 @interface LSIQuakeTests : XCTestCase
 
@@ -44,5 +45,40 @@
     XCTAssertEqualWithAccuracy(-116.7776667, quake.longitude, 0.0001);
 
 }
+
+- (void)testQuakesParsing {
+    NSDate *time = [NSDate dateWithTimeIntervalSince1970:1388620296020 / 1000.0];
+    
+    NSData *quakeData = loadFile(@"Quakes.json", [LSIQuakeTests class]);
+    NSLog(@"quake: %@", quakeData); // Remove print statements in final code, only for "sanity check" when implementing
+    
+    NSError *jsonError = nil;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:quakeData options:0 error:&jsonError];
+    if (jsonError) {
+        NSLog(@"JSON Parsing error: %@", jsonError);
+    }
+    
+    LSIQuakeResults *quakeResults = [[LSIQuakeResults alloc] initWithDictionary:json];
+    XCTAssertNotNil(quakeResults);
+    XCTAssertEqual(2, quakeResults.quakes.count);
+    
+    NSLog(@"Quake Results: %@", quakeResults.quakes);
+    
+    LSIQuake *quake = quakeResults.quakes[0];
+    
+    XCTAssertEqualWithAccuracy(1.29, quake.magnitude, 0.0001);
+    XCTAssertEqualObjects(@"10km SSW of Idyllwild, CA", quake.place);
+    XCTAssertEqualObjects(time, quake.time);
+    
+    // For now we'll just set the alert to nil, you may want to use @"" instead
+//    XCTAssertNil(quake.alert);
+//    
+//    XCTAssertEqualObjects(@"earthquake", quake.type);
+    XCTAssertEqualWithAccuracy(33.663333299999998, quake.latitude, 0.0001);
+    XCTAssertEqualWithAccuracy(-116.7776667, quake.longitude, 0.0001);
+    
+}
+
+
 
 @end
